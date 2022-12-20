e7628442adb1dc3e81990fa5bd23d21f468ac7c3
Gokulraj R <rgokulraj1093@gmail.com>
2022-12-21 00:13:01 +0530

    Update Count.java

--- Count.java
+++ Count.java
@@ -1,20 +1,20 @@
 //string handling
 //7a
-public class Count {
+public class Count{
 	public static void main(String[] args) {
 		String line = "This website is aw3som3.";
 		int vowels = 0, consonants = 0, digits = 0, spaces = 0;
 		line = line.toLowerCase();
-		for (int i = 0; i < line.length(); ++i) {
+		for (int i = 0; i < line.length();i++) {
 			char ch = line.charAt(i);
 			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-				++vowels;
+				vowels++;
 			} else if ((ch >= 'a' && ch <= 'z')) {
-				++consonants;
+				consonants++;
 			} else if (ch >= '0' && ch <= '9') {
-				++digits;
+				digits++;
 			} else if (ch == ' ') {
-				++spaces;
+				spaces++;
 			}
 		}
 		System.out.println("Vowels: " + vowels);
