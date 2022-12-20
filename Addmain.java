a89c218b14b33bebddd78a6247eabc85412239d8
Gokulraj R <rgokulraj1093@gmail.com>
2022-12-21 00:21:49 +0530

    obj changed

--- Addmain.java
+++ Addmain.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 //8
 class Addmain {
 	public static void main(String args[]) {
-		Scanner s = new Scanner(System.in);
-		A obj = new A();
+		Scanner s1 = new Scanner(System.in);
+		A s = new A();
 		System.out.println("enter the value of a");
-		int c = s.nextInt();
+		int c = s1.nextInt();
 		System.out.println("Enter the value of b");
-		int d = s.nextInt();
-		obj.add(c, d);
-		obj.adddisplay();
-		obj.sub(c, d);
-		obj.subdisplay();
-		obj.mul(c, d);
-		obj.muldisplay();
-		obj.div(c, d);
-		obj.divdisplay();
+		int d = s1.nextInt();
+		s.add(c, d);
+		s.adddisplay();
+		s.sub(c, d);
+		s.subdisplay();
+		s.mul(c, d);
+		s.muldisplay();
+		s.div(c, d);
+		s.divdisplay();
 	}
 }
